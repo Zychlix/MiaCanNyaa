0d1d409c918980c821f408cef8d3d044d2273c97
zychlix <mzflighter@gmail.com>
2023-04-23 14:37:14 +0200

    facepalm

---
+++ cmake-build-debug/CMakeFiles/CAN_STM.elf.dir/Core/Src/mia.c.obj.d
@@ -0,0 +1,10 @@
+CMakeFiles/CAN_STM.elf.dir/Core/Src/mia.c.obj: \
+ /home/zychlix/MiaCanNyaa/Core/Src/mia.c \
+ /home/zychlix/MiaCanNyaa/Core/Inc/mia.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h
